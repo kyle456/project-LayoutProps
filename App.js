a04a8712f9b568adc4248c118f631df5a1240928
kyle_kim2123 <ehthffktleh@gmail.com>
2020-09-23 11:11:14 +0900

    add new basic

--- App.js
+++ App.js
@@ -1,1 +1,32 @@
-   
+import React, { useState } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import Constants from 'expo-constants';
+
+
+const App = () => {
+  return (
+    <>
+      <View style={{ paddingTop: Constants.statusBarHeight }} />
+      <View style={[styles.container, styles.playingSpace]}>
+
+      </View>
+
+      <ScrollView>
+
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: '50%',
+  },
+  playingSpace: {
+    backgroundColor: 'white',
+    borderColor: 'blue',
+    borderWidth: 3,
+  },
+})
+
+export default App;
