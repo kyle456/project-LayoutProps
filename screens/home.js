cdab06f2612fca58c4ae676ae5e43fd07bb06750
kyle_kim2123 <ehthffktleh@gmail.com>
2020-11-04 11:22:42 +0900

    add sally image to Home Screen

--- screens/home.js
+++ screens/home.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Image } from 'react-native';
 
 function Home({navigation}) {
     return (
         <>
-            <Text> Hello Home </Text>
+            <Image 
+                source={require('../assets/sally.png')}
+                style={{ width: '100%', height: '50%' }}
+            />
             <Button
                 title="Go to Layout"
                 onPress={() => navigation.navigate('Layout')}
