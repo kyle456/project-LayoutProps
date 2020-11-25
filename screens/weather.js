3560add4bde2a18874d59e7e7a0bcb329e15e0d2
kyle_kim2123 <ehthffktleh@gmail.com>
2020-11-25 12:04:23 +0900

    get weather data

--- screens/weather.js
+++ screens/weather.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Text, Alert, StyleSheet, View } from 'react-native';
 import * as Location from 'expo-location';
+import Axios from 'axios';
+
+const apiKey = process.env.REACT_APP_WEATHER_KEY;
 
 export default class Weather extends React.Component {
     getLocation = async () => {
         try {
             await Location.requestPermissionsAsync();
-            const location = await Location. getCurrentPositionAsync();
+            const location = await Location.getCurrentPositionAsync();
             console.log(location);
+            console.log(location.coords.latitude, location.coords.longitude);
+
+            this.getWeather(location.coords.latitude, location.coords.longitude);
         } catch (error) {
-            Alert.alert("Error", {error});
+            Alert.alert("Error", { error });
         }
+    }
+
+    getWeather = async (latitude, longitude) => {
+        const { data } = await Axios.get(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+        );
+        console.log(data);
+    }
+
+    componentDidMount() {
+        this.getLocation();
     }
 
     render() {
