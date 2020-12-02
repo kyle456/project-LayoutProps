7692239cf51525eb0a296cad46a55685cdfc871f
kyle_kim2123 <ehthffktleh@gmail.com>
2020-12-02 10:02:59 +0900

    render weather

--- screens/weather.js
+++ screens/weather.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Text, Alert, StyleSheet, View } from 'react-native';
 import * as Location from 'expo-location';
 import Axios from 'axios';
+import Constants from 'expo-constants';
 
 const apiKey = process.env.REACT_APP_WEATHER_KEY;
 
 export default class Weather extends React.Component {
+
+    state = {
+        cond: "Clear"
+    };
+
     getLocation = async () => {
         try {
             await Location.requestPermissionsAsync();
@@ -24,6 +30,11 @@ export default class Weather extends React.Component {
             `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
         );
         console.log(data);
+        console.log(data.main.temp); // 온도
+        console.log(data.main.temp_max); // 최고 온도
+        console.log(data.main.temp_min); // 최저 온도
+        console.log(data.weather[0].main); // 날씨(맑음, 흐림 ...)
+        this.setState({ cond: data.weather[0].main, temp: data.main.temp });
     }
 
     componentDidMount() {
@@ -31,8 +42,12 @@ export default class Weather extends React.Component {
     }
 
     render() {
+        const { cond, temp } = this.state;
         return (
             <>
+                <View style={{ paddingTop: Constants.statusBarHeight }} />
+                <Text>{temp}</Text>
+                <Text>{cond}</Text>
             </>
         )
     }
