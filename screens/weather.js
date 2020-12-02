af3665eaf25d8dd4a477f8b6be6925293310ba1b
kyle_kim2123 <ehthffktleh@gmail.com>
2020-12-02 10:27:27 +0900

    apply style to weather data

--- screens/weather.js
+++ screens/weather.js
@@ -3,8 +3,21 @@ import { Text, Alert, StyleSheet, View } from 'react-native';
 import * as Location from 'expo-location';
 import Axios from 'axios';
 import Constants from 'expo-constants';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const apiKey = process.env.REACT_APP_WEATHER_KEY;
+
+const weatherOptions = {
+    Clear: {
+        iconName: "weather-sunny"
+    },
+    Clouds: {
+        iconName: "weather-cloudy"
+    },
+    Thunderstorm: {
+        iconName: "weather-lightning"
+    }
+}
 
 export default class Weather extends React.Component {
 
@@ -43,12 +56,32 @@ export default class Weather extends React.Component {
 
     render() {
         const { cond, temp } = this.state;
-        return (
-            <>
-                <View style={{ paddingTop: Constants.statusBarHeight }} />
-                <Text>{temp}</Text>
-                <Text>{cond}</Text>
-            </>
+        return (         
+            <View style={styles.container}>
+                <View style={styles.halfContainer}>
+                    <MaterialCommunityIcons name={weatherOptions[cond].iconName} size={128} color="black" />
+                    <Text style={styles.tempTitle}>{temp} ℃</Text>
+                </View>
+                <View style={styles.halfContainer}>
+
+                </View>
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    halfContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    tempTitle: {
+        fontSize: 24,
+    },
+})
